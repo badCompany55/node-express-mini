1fc39c3f2d1cd4b98b73ec1879ea63bd856ee5d1
badCompany55 <zachirvin45@gmail.com>
2019-02-26 23:38:16 -0600

    INITIAL SERVER add express, add get route for all users and single user by id

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // implement your API here
+const express = require('express')
+const users = require('./data/db.js')
+const server = express();
+
+const port = 5000
+const parser = express.json()
+
+
+server.use(parser)
+
+server.get('/api/users', (req, res) => {
+	users.find()
+		.then(users => {
+			res.status(200).json(users)
+		})
+		.catch(err => {
+			console.log(err)
+		})
+})
+
+server.get(`/api/users/:id`, (req, res) => {
+	const id = req.params.id
+	users.findById(id)
+		.then(user => {
+			res.status(200).json(user)
+		})
+		.catch(err => {
+			console.log(error)
+		})
+})
+
+server.listen(port, () => {
+	console.log(`Server is running on Port: ${port}`)
+})
+
