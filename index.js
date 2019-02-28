cb3b3fb54a4930fc037f2792921bb1ab78d61ace
badCompany55 <zachirvin45@gmail.com>
2019-02-27 20:15:18 -0600

    POST ERROR add error handler for the post req

--- index.js
+++ index.js
@@ -1,67 +1,74 @@
 // implement your API here
-const express = require('express')
-const users = require('./data/db.js')
+const express = require("express");
+const users = require("./data/db.js");
 const server = express();
 
-const port = 5000
-const parser = express.json()
+const port = 5000;
+const parser = express.json();
 
+server.use(parser);
 
-server.use(parser)
-
-server.get('/api/users', (req, res) => {
-	users.find()
-		.then(users => {
-			res.status(200).json(users)
-		})
-		.catch(err => {
-			console.log(err)
-		})
-})
+server.get("/api/users", (req, res) => {
+  users
+    .find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 server.get(`/api/users/:id`, (req, res) => {
-	const id = req.params.id
-	users.findById(id)
-		.then(user => {
-			res.status(200).json(user)
-		})
-		.catch(err => {
-			console.log(error)
-		})
-})
+  const id = req.params.id;
+  users
+    .findById(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(error);
+    });
+});
 
-server.post('/api/users', (req, res) => {
-	users.insert(req.body)
-		.then(user => {
-			res.status(201).json({success: true})
-		})
-		.catch(err => console.log(err))
-})
+server.post("/api/users", (req, res) => {
+  if (req.body.name && req.body.bio) {
+    users
+      .insert(req.body)
+      .then(user => {
+        res.status(201).json({ success: true });
+      })
+      .catch(err => console.log(err));
+  } else {
+    res.json({ errorMessage: "Please provide name and bio for the user." });
+  }
+});
 
-server.put('/api/users/:id', (req, res) => {
-	const id = req.params.id
-	const updateContent = req.body
-	users.update(id, updateContent)
-		.then(user => {
-			res.status(200).json({success_code: 1})
-		})
-		.catch(err => {
-			console.log(err)
-		})
-})
+server.put("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  const updateContent = req.body;
+  users
+    .update(id, updateContent)
+    .then(user => {
+      res.status(200).json({ success_code: 1 });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-server.delete('/api/users/:id', (req, res) => {
-	const id = req.params.id
-	users.remove(id)
-		.then(user => {
-			res.status(200).json({success_code: user})
-		})
-		.catch(err => {
-			console.log(err)
-		})
-})
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  users
+    .remove(id)
+    .then(user => {
+      res.status(200).json({ success_code: user });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 server.listen(port, () => {
-	console.log(`Server is running on Port: ${port}`)
-})
-
+  console.log(`Server is running on Port: ${port}`);
+});
